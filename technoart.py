2c5eeb73337e2cd7d7aa14a3ca97440da2ae7439
EldosHD <Eldos.sonofelax@gmail.com>
2022-07-03 19:19:13 +0200

    added functions for printig escape codes for rgb colors

--- technoart.py
+++ technoart.py
@@ -1,1 +1,24 @@
+#!/usr/bin/python3
 # converts a random img to pixelart
+block = "▀"
+
+
+def printForground(r, g, b):
+    """Prints out the escape code for the given forground color. That will color the upper pixel."""
+    print(f"\x1b[38;2;{r};{g};{b}mm", end="")
+
+
+def printBackground(r, g, b):
+    """Prints out the escape code for the given background color. That will color the lower pixel."""
+    print(f"\x1b[48;2;{r};{g};{b}m", end="")
+
+
+def printReset():
+    """Prints out the escape code for resetting the color."""
+    print("\x1b[0m", end="")
+
+printForground(173, 148, 111)
+print("hey")
+printReset()
+
+
