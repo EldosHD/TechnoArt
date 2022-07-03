8ef3a7629439772172ab539a91ad5e7e5310445f
EldosHD <Eldos.sonofelax@gmail.com>
2022-07-03 23:28:44 +0200

    added the large technoface

--- technoart.py
+++ technoart.py
@@ -5,7 +5,7 @@ block = "▀"
 
 def printForground(r, g, b):
     """Prints out the escape code for the given forground color. That will color the upper pixel."""
-    print(f"\x1b[38;2;{r};{g};{b}mm", end="")
+    print(f"\x1b[38;2;{r};{g};{b}m", end="")
 
 
 def printBackground(r, g, b):
@@ -18,7 +18,12 @@ def printReset():
     print("\x1b[0m", end="")
 
 printForground(173, 148, 111)
-print("hey")
+printBackground(255, 100, 30)
+print(block, end="")
+printReset()
+printForground(200, 150, 255)
+printBackground(200, 30, 100)
+print(block, end="")
 printReset()
 
 
